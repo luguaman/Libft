0b31999c5f366d3df7b45848b1cfe282436ad8aa
luguaman <148422337+luguaman@users.noreply.github.com>
2023-10-24 15:20:03 +0200

    Add files via upload

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: luguaman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:26:47 by luguaman          #+#    #+#             */
-/*   Updated: 2023/10/19 15:09:20 by luguaman         ###   ########.fr       */
+/*   Updated: 2023/10/24 15:19:13 by luguaman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,67 +14,76 @@
 
 static int	count_digits(int n)
 {
-	int		temp;
 	int		count;
 
-	temp = n;
 	count = 0;
 	if (n == 0)
 		return (1);
-	while (temp > 0)
+	if (n < 0)
+		n *= -1;
+	while (n > 0)
 	{
 		count++;
-		temp = temp / 10;
+		n /= 10;
 	}
 	return (count);
 }
 
-static char	*negative(char *str, int n)
+/*static char	*negative(char *str, int n)
 {
-	int	temp;
+	int	len;
 
-	temp = count_digits(n *= -1);
-	str = malloc(temp + 1);
+	n *= -1;
+	len = count_digits(n) + 2;
+	str = malloc(len);
 	if (!str)
 		return (NULL);
-	str[temp + 1] = '\0';
-	while (temp > 0)
+	str[len--] = '\0';
+	while (n)
 	{
-		str[temp] = n % 10 + '0';
+		len--;
+		str[len] = n % 10 + '0';
 		n = n / 10;
-		temp--;
 	}
-	str[temp] = '-';
+	str[0] = '-';
 	return (str);
-}
+}*/
 
 char	*ft_itoa(int n)
 {
 	char	*str;
-	int		temp;
+	int		len;
 
-	if (n < 0)
-	{
-		str = NULL;
-		str = negative(str, n);
-		return (str);
-	}
-	temp = count_digits(n);
-	str = malloc(temp + 1);
+	len = count_digits(n);
+	str = malloc(len + 1);
 	if (!str)
 		return (NULL);
-	str[temp] = '\0';
-	while (temp--)
+	if (n == 0)
+		str[0] = '0';
+	if (n < 0)
 	{
-		str[temp] = n % 10 + '0';
+		str[0] = '-';
+		n *= -1;
+	}
+	str[len--] = '\0';
+	while (n)
+	{
+		str[len] = n % 10 + '0';
 		n = n / 10;
+		len--;
 	}
 	return (str);
 }
 
 /*int main()
 {
-	int a = 0;
-	printf("Str: %s", ft_itoa(a));
+	printf("0, Str: %s\n", ft_itoa(0));
+	printf("9, Str: %s\n", ft_itoa(9));
+	printf("-9, Str: %s\n", ft_itoa(-9));
+	printf("10, Str: %s\n", ft_itoa(10));
+	printf("-10, Str: %s\n", ft_itoa(-10));
+	printf("8124, Str: %s\n", ft_itoa(8124));
+	printf("-8124, Str: %s\n", ft_itoa(-8124));
+	printf("-9874, Str: %s\n", ft_itoa(-9874));
 	return 0;
 }*/
